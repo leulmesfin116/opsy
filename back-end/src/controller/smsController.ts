4fd98fb7abddba96c7e54878abf77ade002d23a5
leulmesfin116 <leul116hga@gmail.com>
2026-05-18 20:51:49 +0300

    connecting the telegram client

--- back-end/src/controller/smsController.ts
+++ back-end/src/controller/smsController.ts
@@ -4,14 +4,19 @@ import { TelegramClient } from "telegram";
 import { StringSession } from "telegram/sessions";
 const apiId = parseInt(process.env.TELEGRAM_API_ID || "0", 10);
 const apiHash = process.env.TELEGRAM_API_HASH || "";
-const stringSession = new StringSession(sessionString);
+const stringSession = new StringSession();
 
-export const requestCodecontroller = (req: Request, res: Response) => {
+export const requestCodecontroller = async (req: Request, res: Response) => {
   try {
     const number = req.body.PhoneNumber;
     const client = new TelegramClient(stringSession, apiId, apiHash, {
       connectionRetries: 5,
     });
+    await client.connect();
+   const code = await client.sendCode({
+    apiId,
+    apiHash;
+   })
   } catch (error) {
     res.status(501).json({ message: "something went wrong" });
   }
