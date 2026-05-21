e8c35e6b78ad9e82ae02e1d344deabaf3c469b3c
leulmesfin116 <leul116hga@gmail.com>
2026-05-21 14:11:31 +0300

    authethicating using a message

--- back-end/src/controller/tgauthController.ts
+++ back-end/src/controller/tgauthController.ts
@@ -1,12 +1,32 @@
 import { Request, Response } from "express";
 import { loginRegistry } from "../memory/loginregistery";
-import { TelegramClient } from "telegram";
+import { TelegramClient, Api } from "telegram";
+
+interface User {
+  client: TelegramClient;
+  phoneCodeHash: string;
+}
 
 export const tgauthController = async (req: Request, res: Response) => {
   try {
     const { phoneNumber, code } = req.body;
-    const session = loginRegistry.getSession(phoneNumber);
-  } catch (erro) {
+
+    const session = loginRegistry.getSession(phoneNumber) as User | undefined;
+
+    if (!session) {
+      return res.status(400).json({ message: "session expired" });
+    }
+
+    const { client, phoneCodeHash } = session;
+
+    await client.invoke(
+      new Api.auth.SignIn({
+        phoneNumber,
+        phoneCodeHash,
+        phoneCode: code,
+      }),
+    );
+  } catch (error) {
     res.status(500).json({ message: "something went wrong" });
   }
 };
