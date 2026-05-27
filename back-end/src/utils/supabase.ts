45417be07df7d3f9f814a2d379e5f4658799a238
leulmesfin116 <leul116hga@gmail.com>
2026-05-27 11:01:39 +0300

    updating

--- back-end/src/utils/supabase.ts
+++ back-end/src/utils/supabase.ts
@@ -1,6 +1,10 @@
 import { createClient } from "@supabase/supabase-js";
 
-export const supabase = createClient(
-  process.env.SUPABASE_URL!,
-  process.env.SUPABASE_ANON_KEY!,
-);
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseAnonKey = process.env.SUPABASE_ANON_KEY;
+
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error("Missing Supabase environment variables in .env file");
+}
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
