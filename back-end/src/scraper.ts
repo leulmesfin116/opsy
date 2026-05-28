e0d7c7406fb9a1bbc481ba50e45f08188340c0a1
leulmesfin116 <leul116hga@gmail.com>
2026-05-28 15:07:42 +0300

    setting the keyword taxonomy

--- back-end/src/scraper.ts
+++ back-end/src/scraper.ts
@@ -9,25 +9,31 @@ const apiHash = process.env.TELEGRAM_API_HASH || "";
 const sessionString = process.env.TELEGRAM_SESSION || ""; // Save this to .env after first login
 
 // key words
-const keyword = [
-  "hiring",
-  "developer",
-  "fullstack",
-  "frontend",
-  "backend",
-  "softwaredeveloper",
-  "graphic designer",
-  "video editor",
-  "animator",
-  "ui/ux",
-  "marketing",
-  "sales",
-  "social media",
-  "manager",
-  "content creator",
-  "advertisement",
+const remote = [
+  "remote",
+  "wfh",
+  "work from home",
+  "anywhere",
+  " fully-remote",
+  "distributed team",
+  "work from anywhere",
 ];
-
+const onsite = [
+  "onsite",
+  "on-site",
+  " office",
+  "in-office",
+  "in-person",
+  "commute",
+  "stationed",
+];
+const hybrid = [
+  "hybrid",
+  "partially remote",
+  "flexible arrangement",
+  "days a week in office",
+  "remote option",
+];
 const stringSession = new StringSession(sessionString);
 
 (async () => {
