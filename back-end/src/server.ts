e614d8e5525f1b0f08595410da9d736bd849b3e3
leulmesfin116 <leul116hga@gmail.com>
2026-06-05 08:06:12 +0300

    updating

--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -2,6 +2,7 @@ import express from "express";
 import auth from "./Routes/authRoute";
 import smsauth from "./Routes/smsRoute";
 import verify from "./Routes/tgauthRoute";
+import register from "./Routes/preferenceRoute";
 
 const app = express();
 app.use(express.json());
@@ -9,6 +10,7 @@ app.use(express.json());
 app.use("/auth", auth);
 app.use("/smsauth", smsauth);
 app.use("/verify", verify);
+app.use("register", register);
 
 const PORT = 8000;
 
