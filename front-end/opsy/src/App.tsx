d06ee9331b3685a5c39c7b2c28114acbb1ff629a
leulmesfin116 <leul116hga@gmail.com>
2026-06-05 09:08:59 +0300

    updating

--- front-end/opsy/src/App.tsx
+++ front-end/opsy/src/App.tsx
@@ -1,9 +1,6 @@
-import { useState } from "react";
 import Home from "./assets/Home.tsx";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <Home></Home>
--- front-end/opsy/src/assets/Home.tsx
+++ front-end/opsy/src/assets/Home.tsx
@@ -1,4 +1,9 @@
 function Header() {
-  return <></>;
+  return (
+    <>
+      <h1>opys</h1>
+      <h2>Get notified instantly when a job matches your skills with Opsy</h2>
+    </>
+  );
 }
 export default Header;
